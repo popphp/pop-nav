889e8363b257c03c1b9944f66325b00fa3e62439
Nick Sagona <nick@nolainteractive.com>
2015-02-11 14:27:54 -0600

    Update copyright

--- src/Nav.php
+++ src/Nav.php
@@ -6,7 +6,7 @@
  * @category   Pop
  * @package    Pop_Nav
  * @author     Nick Sagona, III <dev@nolainteractive.com>
- * @copyright  Copyright (c) 2009-2014 NOLA Interactive, LLC. (http://www.nolainteractive.com)
+ * @copyright  Copyright (c) 2009-2015 NOLA Interactive, LLC. (http://www.nolainteractive.com)
  * @license    http://www.popphp.org/license     New BSD License
  */
 
@@ -26,7 +26,7 @@ use Pop\Dom\Child;
  * @category   Pop
  * @package    Pop_Nav
  * @author     Nick Sagona, III <dev@nolainteractive.com>
- * @copyright  Copyright (c) 2009-2014 NOLA Interactive, LLC. (http://www.nolainteractive.com)
+ * @copyright  Copyright (c) 2009-2015 NOLA Interactive, LLC. (http://www.nolainteractive.com)
  * @license    http://www.popphp.org/license     New BSD License
  * @version    2.0.0a
  */
